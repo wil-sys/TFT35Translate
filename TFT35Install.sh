87f0adb5bfea865bd7ebe4aaf5150d9ee594b34a
wil-sys <52981362+wil-sys@users.noreply.github.com>
2024-01-21 00:34:08 -0500

    Add files via upload

--- TFT35Install.sh
+++ TFT35Install.sh
@@ -5,8 +5,23 @@ sudo python3 -m /home/"$USER"/TFT35Translate/venv
 sudo /home/"$USER"/TFT35Translate/venv/bin/pip install pyserial
 sudo /home/"$USER"/TFT35Translate/venv/bin/pip install requests
 sudo adduser "$USER" dialout
+echo TFT35Translate adding service files...
+touch Start.sh
+cat << EOF > Start.sh
+#!/bin/bash
+/usr/bin/python /home/"$USER:/TFT35Translate/TFT35.py
+EOF
+sudo chmod a+x Start.sh
+touch Stop.sh
+cat << EOF > Stop.sh
+#!/bin/bash
+for KILLPID in 'ps ax | grep "TFT35" | awk "{print $1;}"'; do
+kill -9 $KILLPID;
+done
+EOF
+sudo a+x Stop.sh
 echo TFT35Translate Starting service...
+sudo systemctl enable /etc/systemd/system/TFT35Translate.service
 sudo systemctl daemon-reload
-sudo systemctl enable TFT35Translate.service
-sudo systemctl start TFT35Translate.service
+sudo TFT35Translate.service start
 echo TFT35Translate install finished!
