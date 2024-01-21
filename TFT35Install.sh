a66bced24decf651843eff1a65d55a79bc2d4f42
wil-sys <52981362+wil-sys@users.noreply.github.com>
2024-01-21 00:09:36 -0500

    Add files via upload

--- TFT35Install.sh
+++ TFT35Install.sh
@@ -6,8 +6,7 @@ sudo /home/"$USER"/TFT35Translate/venv/bin/pip install pyserial
 sudo /home/"$USER"/TFT35Translate/venv/bin/pip install requests
 sudo adduser "$USER" dialout
 echo TFT35Translate Adding python script as service...
-sudo echo > /etc/systemd/system/TFT35Translate.service
-sudo cat <<EOF >/etc/systemd/system/TFT35Translate.service
+sudo bash -c cat <<EOF >/etc/systemd/system/TFT35Translate.service
 [Unit]
 Description=TFT35 -> Moonraker translation service by Wil-Sys
 After=multi-user.target[Service]
