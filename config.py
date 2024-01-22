297c66501e5e6dfd20711a29f73527fa211ff6ee
wil-sys <52981362+wil-sys@users.noreply.github.com>
2024-01-22 03:28:16 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-#These are settings that cannot be auto-configured efficently
+#These are settings that cannot be auto-configured efficently due to the nature of the program
+#Make sure to edit these to be accurate to your printer configuration
 MoonrakerURL= "http://192.168.2.46:7125"
 StepsPerMM = {
     "X": "80",
